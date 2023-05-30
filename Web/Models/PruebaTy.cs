6abfd2855b556f3092654e6928664d1ac642d5ac
LSElias <eliasrodguer12@gmail.com>
2023-05-30 00:43:07 -0600

    * Adición Prueba T

---
+++ Web/Models/PruebaTy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models
+{
+    public class PruebaTy
+    {
+        public int Ty { get; set; }
+    }
+}
