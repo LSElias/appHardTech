08bd37fc7101ad255faa473e0d9e295b9b24e02c
Tylinn <tgonzalezb@est.utn.ac.cr>
2023-06-05 01:51:47 -0600

    Agregación de linea de DB

--- Web/Global.asax.cs
+++ Web/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Web.Utils;
 
 namespace Web
 {
@@ -20,6 +21,15 @@ namespace Web
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
             log4net.Config.XmlConfigurator.Configure();
+        }
+
+        protected void Application_Error()
+        {
+            var ex = Server.GetLastError();
+
+            //  Control de bitácora de errores con log4Net ..
+            Log.Error(ex, System.Reflection.MethodBase.GetCurrentMethod());
+
         }
     }
 }
