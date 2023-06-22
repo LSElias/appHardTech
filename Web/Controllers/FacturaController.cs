163cee87a35d2c90649b02a49411b86d6e278c3b
LSElias <eliasrodguer12@gmail.com>
2023-06-22 04:25:41 -0600

    *  Listado, Listado Proveedor, Detalle *  FacturaController

---
+++ Web/Controllers/FacturaController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class FacturaController : Controller
+    {
+        // GET: Factura
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Factura/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Factura/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Factura/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Factura/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Factura/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Factura/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Factura/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- Web/Controllers/ProductoController.cs
+++ Web/Controllers/ProductoController.cs
@@ -43,6 +43,7 @@ namespace Web.Controllers
         {
             return View();
         }
+
         public ActionResult LoadData()
         {
             try
@@ -90,6 +91,66 @@ namespace Web.Controllers
 
             }
             catch (Exception ex) {
+                Log.Error(ex, MethodBase.GetCurrentMethod());
+                TempData["Message"] = "Error al procesar los datos!" + ex.Message;
+                return RedirectToAction("Default", "Error");
+            }
+        }
+
+
+        public ActionResult IndexProveedor()
+        {
+            return View();
+        }
+
+        public ActionResult LoadDataProveedor(int id)
+        {
+            try
+            {
+                using (MyContext _context = new MyContext())
+                {
+                    var draw = Request.Form.GetValues("draw").FirstOrDefault();
+                    var start = Request.Form.GetValues("start").FirstOrDefault();
+                    var length = Request.Form.GetValues("length").FirstOrDefault();
+                    var sortColumn = Request.Form.GetValues("columns[" +
+                        Request.Form.GetValues("order[0][column]").FirstOrDefault() + "][name]").FirstOrDefault();
+                    var sortColumnDir = Request.Form.GetValues("order[0][dir]").FirstOrDefault();
+                    var searchValue = Request.Form.GetValues("search[value]").FirstOrDefault();
+
+                    // Paginación (tamaño)
+                    int pageSize = length != null ? Convert.ToInt32(length) : 0;
+                    int skip = start != null ? Convert.ToInt32(start) : 0;
+                    int recordsTotal = 0;
+
+                    // Obtención de datos
+                    var productoData = (from p in _context.Producto where p.IdProveedor == id
+                                        join c in _context.Categoria on p.IdCategoria equals c.Id
+                                        join e in _context.Estado on p.Estado equals e.Id
+                                        select new { p.Id, Estado = e.Nombre, p.Precio, p.Nombre, Categoria = c.Nombre });
+
+                    // Organización
+                    if (!(string.IsNullOrEmpty(sortColumn) && string.IsNullOrEmpty(sortColumnDir)))
+                    {
+                        productoData = productoData.OrderBy(sortColumn + " " + sortColumnDir);
+                    }
+                    // Buscar
+                    if (!string.IsNullOrEmpty(searchValue))
+                    {
+                        productoData = productoData.Where(m => m.Nombre.ToLower().Contains(searchValue.ToLower()));
+                    }
+
+                    // Numero total de columnas
+                    recordsTotal = productoData.Count();
+                    //Paginación
+                    var data = productoData.Skip(skip).Take(pageSize).ToList();
+                    // Retorno del JSON
+                    return Json(new { draw = draw, recordsFiltered = recordsTotal, recordsTotal = recordsTotal, data = data });
+
+                }
+
+            }
+            catch (Exception ex)
+            {
                 Log.Error(ex, MethodBase.GetCurrentMethod());
                 TempData["Message"] = "Error al procesar los datos!" + ex.Message;
                 return RedirectToAction("Default", "Error");
