2c9aee58342ba69d05fdd0379e6a05d9f0a0ddf0
LSElias <eliasrodguer12@gmail.com>
2023-06-22 21:37:55 -0600

    * Cambios

--- Infraestructure/Models/Evaluacion.cs
+++ Infraestructure/Models/Evaluacion.cs
@@ -11,7 +11,7 @@ namespace Infraestructure.Models
 {
     using System;
     using System.Collections.Generic;
-    
+     
     public partial class Evaluacion
     {
         public int Id { get; set; }
