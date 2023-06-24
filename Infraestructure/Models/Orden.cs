42c7f540863a9ade5331e94b504900797f766f01
LSElias <eliasrodguer12@gmail.com>
2023-06-24 13:27:33 -0600

    * Cambios

--- Infraestructure/Models/Orden.cs
+++ Infraestructure/Models/Orden.cs
@@ -11,7 +11,7 @@ namespace Infraestructure.Models
 {
     using System;
     using System.Collections.Generic;
-    
+
     public partial class Orden
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
