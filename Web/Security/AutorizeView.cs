726019d699b75a8cbc0d8936941691cb7fb9f01d
LSElias <eliasrodguer12@gmail.com>
2023-07-21 18:11:30 -0600

    * security

---
+++ Web/Security/AutorizeView.cs
@@ -0,0 +1,31 @@
+﻿using Infraestructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Security
+{
+    public class AutorizeView
+    {
+        public static bool IsUserInRole(string[] nombreRoles)
+        {
+            IEnumerable<Roles> allowedroles = nombreRoles.
+                Select(a => (Roles)Enum.Parse(typeof(Roles), a));
+            bool authorize = false;
+            var oUsuario = (Usuario)HttpContext.Current.Session["User"];
+            if (oUsuario != null)
+            {
+                foreach (var role in oUsuario.TipoUsuario) { 
+                    foreach (var rol in allowedroles)
+                    {
+                        if ((int)rol == role.Id )
+                        return true;
+                    }
+                }
+            }
+            return authorize;
+        }
+    }
+
+}
---
+++ Web/Security/CustomAuthenticationFilter.cs
@@ -0,0 +1,38 @@
+﻿using Infraestructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc.Filters;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Web.Security
+{
+    public class CustomAuthenticationFilter : ActionFilterAttribute, IAuthenticationFilter
+    {
+        //Método para autenticar la solicitud
+        public void OnAuthentication(AuthenticationContext filterContext)
+        {
+            if ((Usuario)filterContext.HttpContext.Session["User"] == null)
+            {
+                filterContext.Result = new HttpUnauthorizedResult();
+            }
+        }
+        //Método que se llama cuando falla la autenticación o autorización y 
+        //se llama después del método de ejecución de acción, pero antes de renderizar la vista.
+        public void OnAuthenticationChallenge(AuthenticationChallengeContext filterContext)
+        {
+            if (filterContext.Result == null || filterContext.Result is HttpUnauthorizedResult)
+            {
+                // Redirija al Controller Login
+                filterContext.Result = new RedirectToRouteResult(
+                new RouteValueDictionary
+                {
+                 { "controller", "Login" },
+                 { "action", "Index" }
+                 });
+            }
+        }
+    }
+}
