e1ae005a5277bafe62ad406c1b161355904b5216
LSElias <eliasrodguer12@gmail.com>
2023-07-22 21:31:23 -0600

    - Validacioens de Usuario y Unauthorized

--- Web/Controllers/FacturaController.cs
+++ Web/Controllers/FacturaController.cs
@@ -9,24 +9,32 @@ using System.Web.Mvc;
 using log4net.Util.TypeConverters;
 using Web.Utils;
 using ApplicationCore.Services;
+using Web.Security;
 
 namespace Web.Controllers
 {
     public class FacturaController : Controller
     {
         // GET: Factura
-        public ActionResult IndexAdmin()
-        {
-            return View();
-        }
 
+
+        [CustomAuthorize((int)Roles.Cliente)]
         public ActionResult MisOrdenes()
         {
             return View();
         }
 
-        public ActionResult LoadData(int id)
+        public ActionResult LoadData()
         {
+            var _idUsuario = 0;
+            if (Session["User"] != null)
+            {
+                Usuario oUsuario = (Usuario)Session["User"];
+                if (oUsuario != null)
+                {
+                    _idUsuario = oUsuario.Id;
+                }
+            }
             try
             {
                 using (MyContext _context = new MyContext())
@@ -50,7 +58,7 @@ namespace Web.Controllers
                                         from u in _context.Usuario
                                         from e in _context.Estado
 
-                                        where f.IdUsuario == id
+                                        where f.IdUsuario == _idUsuario
                                         where f.IdOrden == o.IdOrden
                                         where o.IdEstado == e.Id
                                         where f.IdUsuario == u.Id
@@ -87,13 +95,25 @@ namespace Web.Controllers
                 return RedirectToAction("Default", "Error");
             }
         }
+        
+        [CustomAuthorize((int)Roles.Proveedor)]
         public ActionResult MisVentas()
         {
             return View();
         }
 
-        public ActionResult LoadVentas(int id)
+        public ActionResult LoadVentas()
         {
+            var _idUsuario = 0;
+            if (Session["User"] != null)
+            {
+                Usuario oUsuario = (Usuario)Session["User"];
+                if (oUsuario != null)
+                {
+                    _idUsuario = oUsuario.Id;
+                }
+            }
+
             try
             {
                 using (MyContext _context = new MyContext())
@@ -120,13 +140,13 @@ namespace Web.Controllers
                                         from e in _context.Estado
                                         from f in _context.Factura
 
-                                        where p.IdProveedor == id
+                                        where p.IdProveedor == _idUsuario
                                         where p.IdProducto == od.IdProducto
                                         where od.IdOrden == o.IdOrden
                                         where o.IdOrden == f.IdOrden
                                         where od.IdEstado == e.Id
 
-                                        select new { Id = f.IdFactura, Producto = p.Nombre , od.Cantidad, o.SubTotal, Estado = e.Nombre }); ;
+                                        select new { Id = f.IdFactura, f.Fecha , Producto = p.Nombre , od.Cantidad, Estado = e.Nombre }); ;
                     //   join o in _context.Orden on f.IdOrden equals o.Id
                     // join u in _context.Usuario on f.IdUsuario equals u.Id
                     //  join e in _context.Estado on o.Estado equals e.Id
@@ -156,6 +176,8 @@ namespace Web.Controllers
         }
 
         // GET: Factura/Details/5
+
+        [CustomAuthorize((int)Roles.Cliente, (int)Roles.Proveedor, (int)Roles.Administrador)]
         public ActionResult Detalle(int? id)
         {
 
