3977efb97307aed5513c85153583900027a07efc
LSElias <eliasrodguer12@gmail.com>
2023-08-19 15:25:22 -0600

    * Respuestas, Dashboard controller

---
+++ Web/Controllers/DashboardController.cs
@@ -0,0 +1,124 @@
+﻿using ApplicationCore.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web.ViewModel;
+
+namespace Web.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: Dashboard
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Administrador()
+        {
+            IServiceOrden _ServiceOrden = new ServiceOrden();
+            ViewModelGrafico grafico = new ViewModelGrafico();
+            _ServiceOrden.GetOrdenByDia(out string etiquetas, out string valores);
+            grafico.Etiquetas = etiquetas;
+            grafico.Valores = valores;
+            int cantidadValores = valores.Split(',').Length;
+            grafico.Colores = string.Join(",", grafico.GenerateColors(cantidadValores));
+            grafico.tituloEtiquetas = "Compras realizadas durante la semana";
+            //Tipos: bar , bubble , doughnut , pie , line , polarArea 
+            grafico.tipo = "pie";
+            ViewBag.OrdenByDia = grafico;
+
+
+
+
+
+
+            return View();
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+        // GET: Dashboard/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Dashboard/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Dashboard/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Dashboard/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Dashboard/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Dashboard/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Dashboard/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
